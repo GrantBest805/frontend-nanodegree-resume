6325d9bc3c2bbba8e5e2d9d483efa68250a3eddd
Grant Best <grantbst@gmail.com>
2017-08-02 18:55:10 -0700

    First final submition

--- js/helper.js
+++ js/helper.js
@@ -1,24 +1,11 @@
-/*
 
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLlinkedin = '<li class="flex-item"><span class="orange-text">linkedin</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
@@ -30,27 +17,27 @@ var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkEmployer = '<a >%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkDescription = '<p>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<a >%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectDescription = '<p>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a>%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineTitle = '<a class="me">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
@@ -59,9 +46,7 @@ var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 
-/*
-The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
+
 $(document).ready(function() {
   $('button').click(function() {
     var $name = $('#name');
@@ -70,9 +55,7 @@ $(document).ready(function() {
   });
 });
 
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
-*/
+
 var clickLocations = [];
 
 function logClicks(x,y) {
@@ -86,21 +69,17 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  logClicks(x,y);
 });
 
 
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
 var map;    // declares a global map variable
 
 
 /*
-Start here! initializeMap() is called when page is loaded.
+ initializeMap() is called when page is loaded.
 */
 function initializeMap() {
 
@@ -110,16 +89,12 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /*
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js.
-  */
+
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
   /*
   locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
   */
   function locationFinder() {
 
@@ -130,17 +105,13 @@ function initializeMap() {
     locations.push(bio.contacts.location);
 
     // iterates through school locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide:
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    // the locations array.
     education.schools.forEach(function(school){
       locations.push(school.location);
     });
 
     // iterates through work locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide:
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    // the locations array.
     work.jobs.forEach(function(job){
       locations.push(job.location);
     });
@@ -175,9 +146,8 @@ function initializeMap() {
       content: name
     });
 
-    // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+
     });
 
     // this is where the pin actually gets added to the map.
@@ -239,11 +209,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+  // Make sure the map bounds get updated on page resize
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,217 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//  Bio Object
+var bio = {
+	"name": "Grant Best",
+	"role": "Full Stack Web Developer",
+	"skills": ["JavaScript", "Node.js", "Angular", "HTML5", "CSS3", "Bootstrap", "Ruby on Rails", ""],
+	"contacts": {
+		"mobile": "801-556-1841",
+		"email": "grantstoolbox@gmail.com",
+		"github": "GrantBest805",
+		"linkedin": "grant-best",
+		"location": "Ventura, California"
+	},
+	"welcome": "Welcome to my resume dynamically created with JavaScript ! I am a Entrepreneurial minded developer with the ability to create fully responsive Web Sites and Applications, from Design to Database Structure.",
+	"bioPic": "images/Linkedin_Pro.jpg",
+	display: () => {
+		var formattedName = HTMLheaderName.replace("%data%", bio.name);
+		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+		var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+		var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+
+		$("#header").prepend(formattedRole);
+		$("#header").prepend(formattedName);
+		$("#header").append(formattedBioPic);
+		$("#header").append(formattedMessage);
+
+
+		var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+		var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+		var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+		var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkedin);
+		var formattedContact = formattedMobile + formattedEmail + formattedGithub + formattedLinkedin;
+		$("#topContacts").append(formattedContact);
+
+		if(bio.skills.length > 0) {
+			$("#header").append(HTMLskillsStart);
+			for( i in bio.skills) {
+				var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+				$("#skills").append(formattedSkill);
+			}
+		}
+	}
+}
+// Display Bio
+bio.display();
+
+//  Work Experience Object
+var work = {
+	jobs: [
+		{
+			"employer": "Best Flooring, inc.",
+			"title": "Founder/Owner",
+			"dates": "April 2008 - Jan 2017",
+			"location": "Salt Lake City, Utah",
+			"description": "At Best Fooring, inc. Being one of Utahs Top Flooring Companies. Our motto was, Customers First! We also pride oureselves of our attention to detail. My responsibilities as Owner - <br>•Marketing •Business Startup • Sales • Design • Customer Service • Project Management"
+		},
+		{
+			"title": "Co-Founder",
+			"employer": "Best Builders & Design, inc.",
+			"dates": "July 2013 - October 2015",
+			"location": "Salt Lake City, Utah",
+			"description": "Home Remodel and Design Company. Responsibilities - <br>•Marketing •Design •Business Startup •Customer Satisfaction •Project Management"
+		},
+		{
+			"title": "Co-Founder",
+			"employer": "Green Coastal Cleaning",
+			"dates": "Feb 2017",
+			"location": "Ventura, California",
+			"description": "Green Coastal Cleaning is a Residential Home Cleaning Company I Helped Start.Responsibilities - <br> •Marketing •Startup"
+		}
+	],
+	display: () => {
+		$("#workExperience").append(HTMLworkStart);
+		for (var x = 0; x < work.jobs.length; x++) {
+			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[x].employer);
+			var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[x].title);
+			var formattedEmployerTitle = formattedEmployer + formattedTitle;
+			$(".work-entry").append(formattedEmployerTitle);
+			var formattedDates = HTMLworkDates.replace("%data%", work.jobs[x].dates);
+			var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[x].description);
+			var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[x].location);
+			$(".work-entry").append(formattedLocation);
+			var formattedWorkInfo = formattedDates + formattedDescription;
+			$(".work-entry").append(formattedWorkInfo);
+
+		}
+	}
+}
+// Display Work History
+work.display();
+
+// Education Object
+var education = {
+	schools: [
+		{
+			"name": "Coding Dojo",
+			"dates": "February 2017 - July 2017",
+			"degree": "Full Stack Development Certificate",
+
+			"majors": "Web Development",
+			"location": "Burbank, California",
+		},
+		{
+			"name": "Udacity",
+			"dates": "June 2017 - August 2017",
+			"degree": "Front-End nanodegree",
+			"majors": "Front-End Web Development",
+			"location": "Ventura, California",
+		},
+	],
+	onlineCourses: [
+		{
+			"title": "Responsive Websites",
+			"school": "Udacity",
+			"dates": "June 2017",
+			"url": ""
+		},
+		{
+			"title": "JavaScript ",
+			"school": "Udacity",
+			"dates": "July 2017",
+			"url": ""
+		},
+		{
+			"title": "Responsive Images",
+			"school": "Udacity",
+			"dates": "July 2017",
+			"url": ""
+		},
+		{
+			"title": "Version Control , Git Github",
+			"school": "Udacity",
+			"dates": "June 2017",
+			"url": "",
+		},
+	],
+	display: () => {
+		$("#education").append(HTMLschoolStart);
+		for(var i = 0; i < education.schools.length; i++) {
+
+			formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name);
+			formattedSchoolDate = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+			formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+			formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
+			$(".education-entry:last").append(formattedSchoolName);
+			$(".education-entry:last").append(formattedSchoolDate);
+			$(".education-entry:last").append(formattedSchoolLocation);
+			$(".education-entry:last").append(formattedMajor);
+
+		}
+		$(".education-entry:last").append(HTMLonlineClasses);
+		for(var j = 0; j < education.onlineCourses.length; j++) {
+			formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[j].title);
+			formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[j].school);
+			formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[j].dates);
+			$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+			$(".education-entry").append(formattedOnlineDates);
+
+		}
+	}
+}
+// Display education
+education.display();
+
+
+var projects = {
+	myProjects: [
+		{
+			"title": "Dock It",
+			"date": "May 2017",
+			"description": "Dock_It is a working mockup of a scheduling app for small businesses. The Idea is to get rid of the clipboard hanging in the break room. Create a Group for your company, give your employees the group ID and thats it! Now the Group admin can create Positions and fill those positions with events. (Built with Ruby on Rails using RESTFull API) Currently Working on full version, Integrating Full Calendar.",
+		},
+		{
+			"title": "Portfolio Page",
+			"date": "July 2017",
+			"description": "My Portfolio Page Is a Simple, Fully Responsive Design. With the help of a great tools like Gulp and FlexBox this site is Responsive and Minimized, for a Great Development and User experience."
+		},
+		{
+			"title": "MEAN topics",
+			"date": "April 2017",
+			"description": "MEAN Topics is a simple Blog application, built with Node.js, Angluar, Express and MongoDB, using RESTful API's and CRUD operations. Users can create topics, Post on a topic and also Comment on Posts."
+		}
+	],
+	display: () => {
+		for (var p = 0; p < projects.myProjects.length; p++) {
+			$("#projects").append(HTMLprojectStart);
+			var projectTitle = HTMLprojectTitle.replace("%data%", projects.myProjects[p].title);
+			var projectDates = HTMLprojectDates.replace("%data%", projects.myProjects[p].date);
+			var projectDescription = HTMLprojectDescription.replace("%data%", projects.myProjects[p].description);
+			var formattedProject = projectTitle + projectDates + projectDescription;
+			$(".project-entry:last").append(formattedProject);
+		}
+	}
+}
+
+//  Display Projects
+projects.display();
+
+// Footer contacts
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkedin);
+var formattedContact = formattedMobile + formattedEmail + formattedGithub + formattedLinkedin;
+$("#footerContacts").append(formattedContact);
+
+// internationalize Name
+function inName(name) {
+	name = name.trim().split(" ");
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+	return name[0] + " " + name[1];
+}
+$("#main").append(internationalizeButton);
+
+// Display locations with google Map Api
+$("#mapDiv").append(googleMap);
